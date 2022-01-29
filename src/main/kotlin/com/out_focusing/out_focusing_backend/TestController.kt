4c36b70e5d10a44e18358ac2344df96b71168245
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2022-01-29 19:00:39 +0900

    [TEST] Test Controller

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/TestController.kt
@@ -0,0 +1,16 @@
+package com.out_focusing.out_focusing_backend
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    fun getTest(): String {
+        return "Test"
+    }
+
+}
