46a92a57d4e95b3a3f083a3668ee4460c839a16d
사승은 <gsasy0113@gmail.com>
2022-03-21 11:24:51 +0900

    [ADD] Post & Album Repository

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/album/repository/AlbumRepository.kt
@@ -0,0 +1,9 @@
+package com.out_focusing.out_focusing_backend.album.repository
+
+import com.out_focusing.out_focusing_backend.album.domain.Album
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AlbumRepository: CrudRepository<Album, Long> {
+}
