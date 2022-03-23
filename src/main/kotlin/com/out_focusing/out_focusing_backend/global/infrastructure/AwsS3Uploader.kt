828e153bd4d5c7de92a49832543f96811e2b3245
사승은 <gsasy0113@gmail.com>
2022-03-23 16:40:45 +0900

    [ADD] AWS S3 Uploader

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/global/infrastructure/AwsS3Uploader.kt
@@ -0,0 +1,40 @@
+package com.out_focusing.out_focusing_backend.global.infrastructure
+
+import com.amazonaws.HttpMethod
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.Headers
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import com.out_focusing.out_focusing_backend.global.error.CustomException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class AwsS3Uploader(
+    private val amazonS3: AmazonS3,
+    @Value("\${cloud.aws.s3.bucket}")
+    val bucket: String
+) {
+
+    private fun generateUrl(fileName: String, httpMethod: HttpMethod): String {
+        val calendar = Calendar.getInstance()
+        calendar.time = Date()
+        calendar.add(Calendar.HOUR, 1)
+
+        val generatePresignedUrlRequest = GeneratePresignedUrlRequest(bucket, fileName).apply {
+            withMethod(httpMethod)
+            withExpiration(calendar.time)
+            addRequestParameter(Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString())
+        }
+
+        return amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toExternalForm()
+    }
+
+    fun save(extension: String): String {
+        val fileName = UUID.randomUUID().toString() + extension
+        return generateUrl(fileName, HttpMethod.PUT)
+    }
+
+}
