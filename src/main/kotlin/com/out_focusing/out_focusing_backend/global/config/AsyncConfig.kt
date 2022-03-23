aef289f7500e789ab48b52ad976ced582c802bce
사승은 <gsasy0113@gmail.com>
2022-03-23 16:39:38 +0900

    [ADD] Async Configuration

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/global/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package com.out_focusing.out_focusing_backend.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfig
