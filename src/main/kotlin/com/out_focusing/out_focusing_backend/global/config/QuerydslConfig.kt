036550557831128ee756cff1880871f6c7f24f20
사승은 <gsasy0113@gmail.com>
2022-03-24 20:54:28 +0900

    [ADD] Query Setting

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/global/config/QuerydslConfig.kt
@@ -0,0 +1,13 @@
+package com.out_focusing.out_focusing_backend.global.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QuerydslConfig(@PersistenceContext val entityManager: EntityManager) {
+    @Bean
+    fun jpaQueryFactory() = JPAQueryFactory(entityManager)
+}
