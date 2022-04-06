c53f9b1154e119813c50946211a1270177f5ac5c
사승은 <gsasy0113@gmail.com>
2022-04-06 14:41:28 +0900

    [UPDATE] Model

--- src/main/kotlin/com/out_focusing/out_focusing_backend/post/domain/Post.kt
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/post/domain/Post.kt
@@ -3,31 +3,34 @@ package com.out_focusing.out_focusing_backend.post.domain
 import com.out_focusing.out_focusing_backend.album.domain.Album
 import com.out_focusing.out_focusing_backend.user.domain.UserProfile
 import org.hibernate.annotations.CreationTimestamp
-import org.hibernate.annotations.Fetch
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 @Table(name = "post")
+@SQLDelete(sql = "UPDATE post SET deleted = true WHERE post_id=?")
+@Where(clause = "deleted=false")
 class Post(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    val postId: Long,
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "writer_user_id")
     val writerUserProfile: UserProfile,
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "album_id")
     val album: Album,
-    @CreationTimestamp
-    @Column(name = "created_at")
-    val createdAt: LocalDateTime,
     @Column
     val secret: Boolean,
-    @Column
-    val deleted: Boolean,
 ) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    val postId: Long = 0
+    @Column
+    val deleted: Boolean = false
+    @CreationTimestamp
+    @Column(name = "created_at")
+    lateinit var createdAt: LocalDateTime
     @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE])
     val bookmarkUsers: List<PostBookmark> = listOf()
     @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE])
