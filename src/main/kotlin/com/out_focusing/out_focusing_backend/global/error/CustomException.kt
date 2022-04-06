b149873e2d95f3263f1966c6e164e790c48ddd98
사승은 <gsasy0113@gmail.com>
2022-04-06 19:08:09 +0900

    [ADD] Exceptions

--- src/main/kotlin/com/out_focusing/out_focusing_backend/global/error/CustomException.kt
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/global/error/CustomException.kt
@@ -21,10 +21,13 @@ open class CustomException(val code: HttpStatus, override val message: String) :
 
     object AlbumDeleteForbiddenException : ForbiddenException("앨범 삭제 권한이 없습니다.")
     object AlbumUpdateForbiddenException : ForbiddenException("앨범 수정 권한이 없습니다.")
+    object PostDeleteForbiddenException: ForbiddenException("게시글 삭제 권한이 없습니다.")
+    object PostUpdateForbiddenException : ForbiddenException("게시글 수정 권한이 없습니다.")
 
     object UserNotFoundException : NotFoundException("존재하지 않는 유저입니다.")
     object AlbumNotFoundException : NotFoundException("존재하지 않은 앨범입니다.")
     object AlbumBookmarkNotFoundException: NotFoundException("북마크에 추가되지 않은 앨범입니다.")
+    object PostNotFoundException : NotFoundException("존재하지 않는 게시글입니다.")
 
     object UserIdConflictException : ConflictException("중복된 유저 아이디입니다.")
     object AlreadyAlbumBookmarkedException: ConflictException("이미 북마크에 추가한 앨범입니다.")
