31f7a3c8ecf0c6cd9ec2a6036bcb6aa9927facae
사승은 <gsasy0113@gmail.com>
2022-04-06 19:11:30 +0900

    [ADD] Request DTO

--- src/main/kotlin/com/out_focusing/out_focusing_backend/post/dto/ModifyPostRequest.kt
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/post/dto/ModifyPostRequest.kt
@@ -1,8 +1,8 @@
 package com.out_focusing.out_focusing_backend.post.dto
 
-data class GeneratePostRequest(
+data class ModifyPostRequest(
     val albumId: Long,
-    val isSecret: Boolean,
     val contents: LinkedHashSet<String>,
-    val hashtags: LinkedHashSet<String>
+    val hashtags: LinkedHashSet<String>,
+    val isSecret: Boolean
 )
