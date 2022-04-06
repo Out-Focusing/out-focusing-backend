0429d159856004907e1cd500ceef04d68b219ef6
사승은 <gsasy0113@gmail.com>
2022-04-06 19:14:23 +0900

    [FIX] add Repository Annotations

--- src/main/kotlin/com/out_focusing/out_focusing_backend/post/repository/PostContentRepository.kt
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/post/repository/PostContentRepository.kt
@@ -2,6 +2,8 @@ package com.out_focusing.out_focusing_backend.post.repository
 
 import com.out_focusing.out_focusing_backend.post.domain.PostContent
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PostContentRepository: CrudRepository<PostContent, Long> {
 }
--- src/main/kotlin/com/out_focusing/out_focusing_backend/post/repository/PostHashtagRepository.kt
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/post/repository/PostHashtagRepository.kt
@@ -2,6 +2,8 @@ package com.out_focusing.out_focusing_backend.post.repository
 
 import com.out_focusing.out_focusing_backend.post.domain.PostHashtag
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PostHashtagRepository: CrudRepository<PostHashtag, Long> {
 }
