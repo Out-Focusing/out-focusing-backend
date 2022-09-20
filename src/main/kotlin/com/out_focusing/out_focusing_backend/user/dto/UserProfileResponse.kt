abe6df4f0977cfba6b2a73f62c07a525749d8191
사승은 <gsasy0113@gmail.com>
2022-09-20 20:48:52 +0900

    [ADD] User Profile Response

---
+++ src/main/kotlin/com/out_focusing/out_focusing_backend/user/dto/UserProfileResponse.kt
@@ -0,0 +1,34 @@
+package com.out_focusing.out_focusing_backend.user.dto
+
+import com.out_focusing.out_focusing_backend.user.domain.UserProfile
+
+data class UserProfileResponse(
+    val userId: String,
+    val name: String,
+    val contact: String,
+    val profileImage: String,
+    val readme: String,
+    val followers: Long,
+    val followings: Long,
+    val following: Boolean,
+) {
+
+    companion object {
+        fun toUSerProfileResponse(
+            userProfile: UserProfile,
+            myProfile: UserProfile?,
+        ): UserProfileResponse {
+            return UserProfileResponse(
+                userProfile.userId,
+                userProfile.name,
+                userProfile.contact,
+                userProfile.profileImage,
+                userProfile.readme,
+                userProfile.followedUsers.size.toLong(),
+                userProfile.followingUsers.size.toLong(),
+                if (myProfile == null) false else userProfile.followingUsers.any { it == myProfile }
+            )
+        }
+    }
+
+}
